4a22a2b2c6f224ceba3cec70b1b50e9cff63623b
Mauricio David <63173068+MauricioD13@users.noreply.github.com>
2020-11-13 11:33:46 -0500

    Codigo funcional ADC y auto-trigger, problema con la salida serial

--- Arduino/Timer0_prueba/Timer0_prueba.ino
+++ Arduino/Timer0_prueba/Timer0_prueba.ino
@@ -1,27 +1,88 @@
+// Testing interrupt-based analog reading
+// ATMega328p
 #include "timers.h"
-#include "ADC.h"
-void setup() {
+// Note, many macro values are defined in <avr/io.h> and
+// <avr/interrupts.h>, which are included automatically by
+// the Arduino interface
+
+// High when a value is ready to be read
+volatile int readFlag;
+
+// Value to store analog result
+volatile int analogVal;
+
+
+// Initialization
+void setup(){
+  
+  pinMode(7,OUTPUT);
   Serial.begin(9600);
-
-  pinMode(13, OUTPUT);
-
-  limit_count(77, 1024);
-  // Activar CTC
-  char mode[5] = "CTC";
-  timer_mode(mode);
-  config_adc(0);
+  limit_count(77,1024);
+  timer_mode(1);
   timer_interruption('A');
-}
-//Funcion que es llamada por
-void print_LED() {
-  byte result;
   
-  Serial.write(ADCL);
+  ADMUX &= 0xDF; // ADLAR en cero, ajuste a la derecha
+ 
+  ADMUX |= 1 << REFS0; // AVcc para la referencia de voltaje
+
+  ADMUX &= 0xF0;// Limpiar bits para ajustar la entrada
+ 
+  ADMUX |= 0; //Puerto A0
+
+  ADCSRA |= (1<<ADEN); // Activar ADC
+ 
+  ADCSRA |= (1<<ADATE); // Activar auto-trigger
+ 
   
-  Serial.write(ADCH);
+  ADCSRB &= 0xF8; // Limpiar bits para la fuente
+
+  ADCSRB |= (0<<ADTS2|1<<ADTS1|1<<ADTS0); // Fuente de inicio Timer/Couter0 compare match A
+
   
+  ADCSRA |= 0x07; // Prescaler 128, recomendacion ADC por encima de 200k no es confiable
+  //CLKADC = 16MHz/128 = 125Hz
+ 
+  // Set ADIE in ADCSRA (0x7A) to enable the ADC interrupt.
+  // Without this, the internal interrupt will not trigger.
+  ADCSRA |= B00001000;
+ 
+  // Enable global interrupts
+  // AVR macro included in <avr/interrupts.h>, which the Arduino IDE
+  // supplies by default.
+  sei();
+ 
+  // Kick off the first ADC
+  readFlag = 0;
+  // Set ADSC in ADCSRA (0x7A) to start the ADC conversion
+  ADCSRA |= 1<<ADSC;
 }
 
-void loop() {
-  print_LED();
+
+// Processor loop
+void loop(){
+
+  // Check to see if the value has been updated
+  if (readFlag == 1){
+   
+    // Perform whatever updating needed
+   
+    readFlag = 0;
+  }
+ 
+  // Whatever else you would normally have running in loop().
+ 
+}
+
+
+// Interrupt service routine for the ADC completion
+ISR(ADC_vect){
+
+  // Done reading
+  readFlag = 1;
+ 
+  // Must read low first
+  analogVal = ADCL | (ADCH << 8);
+  PORTD |= analogVal <<8; //Salida por el puerto 7
+  
+ 
 }
